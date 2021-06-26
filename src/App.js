6251ad1c38f6aa7e6b7ea29347785bddfe59cd93
Marius <mariusmalaiska@gmail.com>
2021-06-26 18:48:51 +0300

    done

--- src/App.js
+++ src/App.js
@@ -1,22 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [input, setInput] = useState();
+  const [errors, setErrors] = useState([]);
+  const [isWin, setIsWin] = useState("");
+
+  let arr = [];
+
+  const checkTheEnd = (arr, n, i) => {
+    if (i < 0 || i >= n) return false;
+
+    if (i === n - 1) return true;
+
+    return checkTheEnd(arr, n, i + arr[i]);
+  };
+
+  const submit = (e) => {
+    e.preventDefault();
+    if (input) {
+      arr = input.split(`,`).map((x) => +x);
+
+      setErrors("");
+      if (!input.match("^[0-9]+([0-9,])+[0-9]+$")) {
+        setErrors("Please fill in like so 1,2,3,0,3");
+      } else {
+        checkTheEnd(arr, arr.length, arr[0])
+          ? setIsWin("Win !!!")
+          : setIsWin("Lost");
+      }
+    } else {
+      setErrors("Field cannot be empty");
+    }
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h6>
+          Please fill as shown below to find out
+          <br /> if you have reached the end of array
+          <br />
+          e.g. 5,3,2,0
+        </h6>
+        <div className="App-container">
+          <div className="Box">
+            <h3>{isWin}</h3>
+            <h3>{input}</h3>
+          </div>
+          <input
+            onChange={(e) => {
+              setInput(e.target.value);
+              setIsWin("");
+            }}
+            className="Input"
+            type="text"
+            placeholder="e.g. 5,3,2,0"
+          />
+          <button className="Button" onClick={submit}>
+            Check
+          </button>
+        </div>
+        <p className="Error">{errors}</p>
       </header>
     </div>
   );
